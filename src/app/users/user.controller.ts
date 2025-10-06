6cdfc772fbc55eaf09ee4119499fd6b9f64dcf0a
DehBarca <dialebar@gmail.com>
2025-10-05 23:00:27 -0600

    Se añaden rutas y controladores de Users.

--- src/app/users/user.controller.ts
+++ src/app/users/user.controller.ts
@@ -1,6 +1,52 @@
-import { Request, Response } from 'express';
+import User from './user.model';
 
-export function getUsers(req: Request, res: Response) {
+import { Request, Response } from 'express';
+import Status from '../interfaces/Status';
+
+
+export const createUser = async (req: Request, res: Response) => {
+	const { username, email, password, profileImgUrl } = req.body;
+	try {
+		const newUser = new User({ username, email, password, profileImgUrl});
+		await newUser.save();
+		
+		return res.status(Status.CREATED).json({ newUser: newUser});
+	} catch (e) {
+		return res.status(Status.INTERNAL_ERROR).json({ error: 'Server error', e });
+	}
+}
+
+export const getUser = (req: Request, res: Response) => {
 	console.log('User: ', req.user);
 	res.send([]);
+}
+
+export const getUserById = async (req: Request, res: Response) => {
+	const userId = req.params.userId;
+	try {
+		const user = await User.findById(userId);
+		return res.status(Status.SUCCESS).json({ user: user });
+	} catch (e) {
+		return res.status(Status.INTERNAL_ERROR).json({ error: 'Server error', e });
+	}
+}
+
+export const modifyUser = async (req:Request, res: Response) => {
+	const {userId, username, email, password, profileImgUrl} = req.body;
+	try {
+		const user = await User.findByIdAndUpdate(userId, {username, email, password, profileImgUrl}, {new: true});
+		return res.status(Status.SUCCESS).json({ updatedUser: user});
+	} catch (e) {
+		return res.status(Status.INTERNAL_ERROR).json({ error: 'Server error', e });
+	}
+}
+
+export const deleteUser = async (req: Request, res: Response) => {
+	const userId = req.params.userId;
+	try {
+		const deletedUser = await User.findByIdAndDelete(userId);
+		return res.status(Status.SUCCESS).json({deletedUser: deletedUser});
+	} catch (e) {
+		return res.status(Status.INTERNAL_ERROR).json({ error: 'Server error', e });
+	}
 }
