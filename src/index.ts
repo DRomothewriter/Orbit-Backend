02a767d16d75c21994c0280c6b8535f6dae10197
Diego Barraza <129478926+DehBarca@users.noreply.github.com>
2025-10-06 18:50:00 -0600

    Revert "Ft tasks"

--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
-import swaggerJsDoc from 'swagger-jsdoc';
-import { setup, serve } from 'swagger-ui-express';
-import swaggerOptions from '../swagger.config';
-import { dbConnect } from './database';
-
-import routes from './app/routes';
 
 const port = process.env.PORT || 3001;
 
 const app = express();
-app.use(routes);
+app.use(express.json());
 
-app.get('', (req, res) => {
-    res.json({ message: 'api works' });
+
+app.listen(port, () => {
+    console.log(`🚀 Servidor corriendo en http://localhost:${port}`);
+    console.log(`📰 API lista para usar`);
 });
 
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/swagger', serve, setup(swaggerDocs));
-
-dbConnect().then(() => {
-    app.listen(port, () => {
-        console.log(`🚀 Servidor corriendo en http://localhost:${port}`);
-        console.log(`📰 API lista para usar`);
-    })
-}).catch(() => {
-        console.log('Failed to connect to the database')
-    })
-
-
+app.get('/', (req, res) => {
+    res.json({ message: 'api works' });
+});
