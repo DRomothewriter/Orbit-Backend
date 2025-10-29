20d245d9bb4d1bb97023f82576b3597a5380a758
DRomothewriter <diego.romo@iteso.mx>
2025-10-28 22:33:00 -0600

    Socket server

--- src/index.ts
+++ src/index.ts
@@ -7,29 +7,41 @@ import { setup, serve } from 'swagger-ui-express';
 import swaggerOptions from '../swagger.config';
 import { dbConnect } from './database';
 
+import { Server } from 'socket.io';
+import http, { createServer } from 'http';
 import routes from './app/routes';
 
 const port = process.env.PORT || 3001;
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
 app.use(express.json()); //Luego lo pondremos únicamente en las rutas necesarias
 app.use(routes);
 
 app.get('', (req, res) => {
-    res.json({ message: 'api works' });
+	res.json({ message: 'api works' });
 });
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use('/swagger', serve, setup(swaggerDocs));
 
-dbConnect().then(() => {
-    app.listen(port, () => {
-        console.log(`🚀 Servidor corriendo en http://localhost:${port}`);
-        console.log(`📚Servidor corriendo en http://localhost:${port}/swagger/`);
-        console.log(`📰 API lista para usar`);
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
     })
-}).catch(() => {
-        console.log('Failed to connect to the database')
-    })
+})
 
-
+dbConnect()
+	.then(() => {
+		server.listen(port, () => {
+			console.log(`🚀 Servidor corriendo en http://localhost:${port}`);
+			console.log(`📚Servidor corriendo en http://localhost:${port}/swagger/`);
+			console.log(`📰 API lista para usar`);
+		});
+	})
+	.catch(() => {
+		console.log('Failed to connect to the database');
+	});
