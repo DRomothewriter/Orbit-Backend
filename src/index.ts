8df39e7dceda3956b44d992ec7e210a74ed58ac0
DehBarca <dialebar@gmail.com>
2026-05-13 20:25:03 -0600

    Nodemailer

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ import {mediasoupService} from './services/mediasoup.service';
 const app = express();
 app.use(
 	cors({
-		origin: process.env.FRONTEND_URL,
+		origin: process.env.FRONTEND_URL || '*',
 		credentials: true,
 		methods: ['GET', 'POST', 'PUT', 'DELETE'],
 	})
